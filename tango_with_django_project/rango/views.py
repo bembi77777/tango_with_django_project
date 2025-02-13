849c24a1dba5bcb9588a74979e1f91cdd3c0ecdc
“Binderiya <2963376m@student.gla.ac.uk>
2025-02-13 21:54:13 +0000

    chapter8 tested

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -4,7 +4,7 @@ from rango.forms import CategoryForm
 from rango.models import Category, Page
 from .forms import PageForm
 from django.urls import reverse
-
+from django.shortcuts import get_object_or_404, render
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
@@ -16,7 +16,10 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
+    print(request.method)  
+    print(request.user)
     return render(request, 'rango/about.html')
+    
 def show_category(request, category_name_slug):
     context_dict = {}
 
@@ -66,3 +69,17 @@ def add_page(request, category_name_slug):
     
     context_dict = {'form': form, 'category': category}
     return render(request, 'rango/add_page.html', context=context_dict)
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['pages'] = None
+        context_dict['category'] = None
+    
+    return render(request, 'rango/category.html', context=context_dict)
